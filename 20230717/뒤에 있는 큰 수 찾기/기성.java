491b07639b00987aaee9fcc7ba78ca2a7b9d4ef5
LEE KEESUNG <83679871+GNuSeekK@users.noreply.github.com>
2023-07-15 16:23:04 +0900

    Update 기성.java
    프로그래머스_17266_브루트포스(실패)

--- 20230717/뒤에 있는 큰 수 찾기/기성.java
+++ 20230717/뒤에 있는 큰 수 찾기/기성.java
@@ -1,1 +1,17 @@
-// 이런식으로 써주시면 됩니다.
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        for(int i=0; i < numbers.length; i++){
+            answer[i] = -1;
+            int num = numbers[i];
+            for(int j=i+1; j < numbers.length; j++){
+                if (numbers[j] > num){
+                    answer[i] = numbers[j];
+                    break;
+                }
+            }
+            
+        }
+        return answer;
+    }
+}
