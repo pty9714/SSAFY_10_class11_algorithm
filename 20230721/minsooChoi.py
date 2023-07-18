d37832287b38a01ff24309e6182734565c1a3e44
Minsoo Choi <73485743+lucaschoi99@users.noreply.github.com>
2023-07-18 10:21:56 +0900

    백준_12904_백트래킹

---
+++ 20230721/minsooChoi.py
@@ -0,0 +1,29 @@
+answer = []
+
+def addA(word):
+    return word + "A"
+
+def flipAddB(word):
+    return word[::-1] + "B"
+
+def simulate(start, target):
+    if start == target:
+        answer.append(start)
+        return
+    if len(start) >= len(target):
+        return
+    # 백트래킹
+    simulate(addA(start), target)
+    simulate(flipAddB(start), target)
+
+S = input()
+T = input()
+simulate(S, T)
+
+if answer:
+    print(1)
+else:
+    print(0)
+
+# ---
+# 시간초과
