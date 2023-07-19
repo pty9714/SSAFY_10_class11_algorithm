3cac2a78351f9c592af85daca286283ee7d5fde6
GNuSeekk <person813@gmail.com>
2023-07-19 13:04:09 +0900

    백준_12904_그리디

--- 20230721/12904/keesung.py
+++ 20230721/12904/keesung.py
@@ -1,24 +1,13 @@
-from collections import deque
-
 S = input()
 T = input()
 
-words = [T, T[::-1]]
+while len(T) > len(S):
+    if T[-1] == 'A':
+        T = T[:-1]
+    else:
+        T = T[:-1][::-1]
 
-q = deque()
-q.append(S)
-
-result = 0
-while q:
-    tmp = q.popleft()
-    if tmp == T:
-        result = 1
-        break
-    if tmp in words[0]:
-        q.append(tmp + 'A')
-    if tmp in words[1]:
-        q.append(tmp + 'B')
-    
-print(result)
-        
-        
+if S == T:
+    print(1)
+else:
+    print(0)
