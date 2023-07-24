c271b5bccc85e1817d46b594eb053800e30f6f3f
hyeseungS <hyeseung2000@gmail.com>
2023-07-24 13:38:37 +0900

    swea_14962_단순구현

---
+++ 20230724/14692/hyeseung.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class Solution
+{
+	public static void main(String args[]) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+		
+			int N = Integer.parseInt(br.readLine()); 
+            
+			System.out.printf("#%d %s\n", test_case, N % 2 == 0? "Alice" : "Bob");
+
+		}
+	}
+}
