338189e24521675345af47deb5c3560c161e8fd1
신혜승 <hyeseung2000@gmail.com>
2023-07-31 23:58:19 +0900

    백준_2212_반복문

---
+++ 20230731/2212/hyeseung.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReade
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int K = Integer.parseInt(br.readLine());
+		int[] matrix = new int[N];
+		Integer[] diff = new Integer[N - 1];
+		int ans = 0;
+		
+		String[] s = br.readLine().split(" ");
+		for(int i = 0; i < N; i++) {
+			matrix[i] = Integer.parseInt(s[i]);
+		}
+		
+		Arrays.sort(matrix);
+		
+		for(int i = 0; i < N - 1; i++) {
+			diff[i] = matrix[i + 1] - matrix[i];
+		}
+		
+		Arrays.sort(diff);
+		
+		for(int i = 0; i < N - K; i++) {
+			ans += diff[i];
+		}
+		System.out.println(ans);
+	}
+
+}
