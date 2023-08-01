6283911d7245cc2fd288840462b7c349ed242b48
박기련 <giryun@gimsujeongs-MacBook-Air.local>
2023-08-01 23:07:33 +0900

    refactor: 백준_2212_정렬(정답확인)

--- 20230731/2212/giryun.py
+++ 20230731/2212/giryun.py
@@ -1,10 +1,23 @@
 n = int(input())
 k = int(input())
+
+if k >= n:
+    print(0)
+    exit()
 sensor = list(map(int, input().split()))
 sensor.sort()
-dist = [abs(sensor[i] - sensor[i+1]) for i in range(n-1)]
+
+dist = [(sensor[i+1] - sensor[i]) for i in range(n-1)]
 dist.sort()
+
 while(k > 1):
     dist.pop()
     k -= 1
+
 print(sum(dist))
+
+
+"""
+결과
+메모리 : 32276KB, 시간 : 52ms
+"""
