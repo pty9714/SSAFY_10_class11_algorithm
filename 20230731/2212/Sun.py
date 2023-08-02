9415f292d7add81a8c7066f0df7f0e71bd85733c
unknown <pty9714@naver.com>
2023-08-02 09:23:30 +0900

    백준_2212_정렬

---
+++ 20230731/2212/Sun.py
@@ -0,0 +1,12 @@
+n = int(input())
+k = int(input())
+sensor = list(map(int,input().split()))
+sensor.sort()
+
+array = []
+for i in range(0,n-1):
+    array.append(sensor[i+1] - sensor[i])
+
+array.sort()
+
+print(sum(array[:n-k]))
