4c5ae6b259ebca43d0e420a6dd2eb6098a19e806
Hyeonjeong Kim <69911502+smarth2o@users.noreply.github.com>
2023-08-08 18:07:55 +0900

    백준_14719_구현+비교

---
+++ 20230808/python/hyeonjeong.py
@@ -0,0 +1,15 @@
+h, w = map(int, input().split())
+blocks = list(map(int, input().split()))
+
+answer = 0
+
+for i in range(1, w-1):
+    start = max(blocks[:i])
+    end = max(blocks[i+1:])
+    height = min(start, end)
+    if blocks[i] < height:
+        answer += height - blocks[i]
+
+print(answer)
+
+# Python3 31256 kb 44 ms
