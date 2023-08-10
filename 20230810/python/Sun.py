47f697b5ee3a292cc123ffed6134844f0c7dd340
unknown <pty9714@naver.com>
2023-08-10 12:02:02 +0900

    백준_1174_구현

---
+++ 20230810/python/Sun.py
@@ -0,0 +1,20 @@
+# n = int(input())
+# cnt = 11
+# INF = int(1e9)
+# if n<=11:
+#     print(n-1)
+# else:
+#     for i in range(11,INF):
+#         for k in range(len(str(i))-1):
+#             if str(i)[k] <= str(i)[k+1]:
+#                 break
+#         else:
+#             cnt +=1
+            
+#         if cnt == n:
+#             print(i)
+#             break
+# 당연히 시간초과
+
+
+n = int(input())
