44515867c7c13126b620eb81641274333b736e5d
박기련 <giryun@gimsujeongs-MacBook-Air.local>
2023-08-16 00:32:40 +0900

    refactor : swea_1491_구현

--- 20230810/python/giryun.py
+++ 20230810/python/giryun.py
@@ -1,16 +1,10 @@
-t = int(input())
-
-for tc in range(1, t + 1) :
-    min_value = -1
+for t in range(1, int(input()) + 1) :
     n, a, b = map(int, input().split())
+    ans = int(1e9)
     for r in range(1, n + 1) :
         c = 1
         while r * c <= n :
-            value = a * (abs(r - c)) + b * (n - (r * c))
-            if min_value == -1 :
-                min_value = value
-            else :
-                min_value = min(min_value, value)
+            ans = min(ans, a * (abs(r - c)) + b * (n - (r * c)))
             c += 1
 
-    print('#%d %d' % (tc, min_value))
+    print(f"#{t} {ans}")
