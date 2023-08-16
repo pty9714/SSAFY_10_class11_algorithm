0fec21f09148a3f2e4f08658fa30b4317e12e6c3
hyeseungS <hyeseung2000@gmail.com>
2023-08-16 16:10:26 +0900

    프로그래머스_92343_해결

--- 20230812/java/hyeseung.java
+++ 20230812/java/hyeseung.java
@@ -1,54 +1,41 @@
 import java.util.*;
 
 class Solution {
-    public static ArrayList<ArrayList<Integer>> map = new ArrayList<ArrayList<Integer>>();
-    public static int result = 0;
-    public static int[] node;
-
+    public static ArrayList<ArrayList<Integer>> tree = new ArrayList<ArrayList<Integer>>();
+    public static int result = 0, animal[];
     public int solution(int[] info, int[][] edges) {
-         
-        boolean[] visited = new boolean[info.length];
-        node = info;
         for(int i = 0; i < info.length; i++) {
-            map.add(new ArrayList<Integer>());
+            tree.add(new ArrayList<Integer>());
         }
-
-        for(int i = 0; i < edges.length; i++) {
-            map.get(edges[i][0]).add(edges[i][1]);
-            map.get(edges[i][1]).add(edges[i][0]);
+        boolean visited[] = new boolean[info.length];
+        animal = info;
+        for(int[] e : edges) {
+            tree.get(e[0]).add(e[1]);
+            tree.get(e[1]).add(e[0]);
         }
-
         dfs(0, 0, 0, visited);
-
+        
         return result;
     }
     
-    public static void dfs(int num, int sheep, int wolf, boolean[] list) {
-        if(node[num] == 0) {
+    public static void dfs(int parent, int sheep, int wolf, boolean[] list) {
+        if(animal[parent] == 0) {
             sheep++;
         }
-        
-        else if(node[num] == 1) {
+        else if(animal[parent] == 1) {
             wolf++;
         }
-        
-        if(wolf >= sheep) {
-            return;
-        }
-
-        boolean[] newList = list.clone();
-        newList[num] = true;
+        if(sheep <= wolf) return;
+        boolean visited[] = list.clone();
         result = Math.max(result, sheep);
-
-        for(int i = 0; i < newList.length; i++) {
-            if(newList[i] == true) {
-                for(int j = 0; j < map.get(i).size(); j++) {
-                    int temp = map.get(i).get(j);
-                    if(newList[temp] == false) {
-                        dfs(temp, sheep, wolf, newList);
-                    }
-                }
+        visited[parent] = true;
+        
+        for(int i = 0; i < visited.length; i++) {
+            if(visited[i]) {
+                for(int child : tree.get(i)) {
+                    if(!visited[child]) dfs(child, sheep, wolf, visited);
+                }       
             }
         }
-    }
+    }    
 }
