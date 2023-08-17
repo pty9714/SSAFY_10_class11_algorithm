8041a15381795128cbe298f3b7f9df3391ddbc43
hyeseungS <hyeseung2000@gmail.com>
2023-08-17 13:30:46 +0900

    swea_1868_bfs

--- 20230816/java/hyeseung.java
+++ 20230816/java/hyeseung.java
@@ -11,6 +11,7 @@ public class S1868 {
 	public static int ans, N, map[][];
 	public static int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0};
 	public static int dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+	public static boolean visited[][];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -20,25 +21,43 @@ public class S1868 {
 			ans = 0;
 			N = Integer.parseInt(br.readLine());
 			map = new int[N][N];
+			visited = new boolean[N][N];
 			
 			for (int i = 0; i < N; i++) {
 				String s = br.readLine();
 				for (int j = 0; j < N; j++) {
 					map[i][j] = s.charAt(j);
 				}
-			}			
-			boolean flag = false;
+			}		
+			
+			// 주변의 지뢰 개수 세기
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
 					if(map[i][j] != '*') {
-						bfs(i, j);
-						flag = true;
-						break;
+						countMine(i, j);
 					}
 				}
-				if(flag) break;
 			}
-			System.out.println(Arrays.deepToString(map));
+			
+			// 주변 지뢰 없는 곳부터 클릭
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if(!visited[i][j] && map[i][j] == 0) {
+						ans++;
+						bfs(i, j);
+					}
+				}
+			}
+			
+			// 주변 지뢰 없는 곳부터 클릭
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if(!visited[i][j] && map[i][j] != '*' && map[i][j] != 0) {
+						ans++;
+					}
+				}
+			}
+			
 			bw.write("#" + test_case + " " + ans + "\n");
 		}
 		
@@ -50,31 +69,30 @@ public class S1868 {
 	public static void bfs(int x, int y) {
 		Queue<int[]> q = new LinkedList<>();
 		q.offer(new int[] {x, y});
-		boolean visited[][] = new boolean[N][N];
 		while(!q.isEmpty()) {
 			int[] p = q.poll();
-			if(!visited[p[0]][p[1]]) {
-				int temp = 0, cnt = 0;
-				ans++;
-				visited[p[0]][p[1]] = true;
-				for (int i = 0; i < 8; i++) {
-					int tempx = p[0] + dx[i];
-					int tempy = p[1] + dy[i];
-					if(tempx < 0 || tempx >= N || tempy < 0 || tempy >= N) continue;
-					if(map[tempx][tempy] == '*') {
-						temp++;
-					}
-					else if(!visited[tempx][tempy]) {
-						cnt++;
-						System.out.println(tempx + " " + tempy);
-						q.offer(new int[] {tempx, tempy});
-					}
-				}	
-				map[p[0]][p[1]] = temp;
-				System.out.println(p[0] + " " + p[1] + " " + temp + " " + cnt);
-				if(temp == 0) ans -= cnt;
-				System.out.println(ans);
+			visited[p[0]][p[1]] = true;
+			for (int i = 0; i < 8; i++) {
+				int tempx = p[0] + dx[i];
+				int tempy = p[1] + dy[i];
+				if(tempx < 0 || tempx >= N || tempy < 0 || tempy >= N || visited[tempx][tempy]) continue;
+				visited[tempx][tempy] = true;
+				// 주변 칸도 0이면 큐에 넣기
+				if(map[tempx][tempy] == 0) { 
+					q.offer(new int[] {tempx, tempy});
+				}
 			}
 		}
+	}
+	
+	public static void countMine(int x, int y) {
+		int temp = 0;
+		for (int i = 0; i < 8; i++) {
+			int tempx = x + dx[i];
+			int tempy = y + dy[i];
+			if(tempx < 0 || tempx >= N || tempy < 0 || tempy >= N) continue;
+			if(map[tempx][tempy] == '*') temp++;
+		}
+		map[x][y] = temp;
 	}
 }
