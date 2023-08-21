0985aa93f163c8aa4152806cdb37802988aba916
keesung <person813@gmail.com>
2023-08-21 17:05:52 +0900

    프로그래머스_150364_구현

--- 20230821/python/keesung.py
+++ 20230821/python/keesung.py
@@ -11,6 +11,9 @@ def solution(edges, target):
     for key in tree.keys():
         tree[key].sort()
     now_node = [0] * (len(target) + 1)
+    
+    # 싸이클이 있는 순서 배열 만들기
+    # 싸이클 생기면 바로 break 해서 order 배열을 통해서 구현
     order = []
     while True:
         node = 1
@@ -23,32 +26,44 @@ def solution(edges, target):
             node = now_node[node]
         if len(order) % 2 == 0 and order[:len(order) // 2] == order[len(order) // 2:]:
             order = order[:len(order) // 2]
-            break
+            break    
+    
+    # 최소한 1은 더해져야 하며, 최대 3까지 더해줄 수 있으므로 배열 두개 생성하여 최소 최대 구성
     min_target = [0] * len(target)
     max_target = [0] * len(target)
     arr = [False] * len(target)
     for i in range(1, len(target)):
         if target[i] == 0:
+            # 이미 0인 곳은 완료된 것으로 판단
             arr[i] = True
         elif i not in order:
+            # 0이 아닌데 싸이클에 있으면 불가능한 것
             return [-1]
+        
     signal = True
     while signal:
         for num in order:
+            # 순서대로 더해줌
             min_target[num] += 1
             max_target[num] += 3
             if min_target[num] > target[num]:
+                # 더해주다가 min값이 target 넘어버리면 불가능하므로 [-1] 반환
                 return [-1]
             if target[num] <= max_target[num]:
+                # max값이 target보다 크면 만들 수 있는 숫자이므로
                 arr[num] = True
             # print(arr)
             if sum(arr) == len(arr) - 1:
+                # 모두다 True이면 완료
                 signal = False
                 break
+    
+    # 각 노드별로 최소 갯수 만큼 [1] * 해서 배열 생성
     answer = [[1] * x for x in min_target]
     for index, ans in enumerate(answer):
         new_index = 0
         if ans != []:
+            # 빈 배열이 아닐때 처음 숫자부터 1씩 더해서 3을 만들어줌
             while sum(ans) < target[index]:
                 # print(index, new_index, ans)
                 if ans[new_index] == 3:
@@ -60,9 +75,12 @@ def solution(edges, target):
     while signal:
         for num in order:
             if answer[num] == []:
+                # 빈배열 나오면 끝난 것
                 signal = False
                 break
+            # 빈 배열이 아니면 마지막에서 1개씩 빼서 다시 새로운 배열에 넣어줌
             new_answer.append(answer[num].pop())
+    # answer 초기화
     answer = new_answer
     
     return answer
