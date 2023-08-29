708b376c2160b8e88c06152533cf0d575a11f804
hyeseungS <hyeseung2000@gmail.com>
2023-08-29 12:02:46 +0900

    swea_15231_구현

--- 20230828/java/hyeseung.java
+++ 20230828/java/hyeseung.java
@@ -7,29 +7,38 @@ import java.util.StringTokenizer;
 
 public class S15231_바이너리트리_신혜승 {
 
-public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    StringTokenizer st;
-    
-    int T = Integer.parseInt(br.readLine());
-    
-    for (int test_case = 1; test_case <= T; test_case++) {
-        int ans = 0;
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int V = Integer.parseInt(st.nextToken());
-        
-        while(V > 1) {
-            ans++;
-            V /= 2;
-        }
-        ans += (Math.log(N) / Math.log(2));
-        bw.write("#" + test_case + " " + ans + "\n");
-    }
-    
-    bw.flush();
-    bw.close();
-    br.close();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int ans = 0;
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int V = Integer.parseInt(st.nextToken());
+			
+			// 노드 v와 루트까지의 간선 개수와 이진 트리의 높이
+			ans = (int) (Math.log(V) / Math.log(2)) + (int) (Math.log(N) / Math.log(2));
+			// 부모가 2인 서브트리인지 3인 서브트리인지 구함
+			while(V > 3) V /= 2;
+			while(N > 3) N /= 2;
+			if(V == 2) {
+				if(N == 2) {
+					ans--;
+				}
+			}
+			
+			bw.write("#" + test_case + " " + ans + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
 }
-}
+
+// 68,020 kb, 290 ms
