13cc69aa5a7b5a72ef2aa9b21dbd588dcd0f274a
hyeseungS <hyeseung2000@gmail.com>
2023-08-30 14:32:33 +0900

    백준_9251_dp

--- 20230823/java/hyeseung.java
+++ 20230823/java/hyeseung.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 // 16132KB, 116ms	
-public class B9251 {
+public class Main {
 	
 	public static int sub[][];
 
@@ -28,11 +28,11 @@ public class B9251 {
 	private static int findSubSeuquence(String s1, String s2) {
 		for (int i = 1; i <= s1.length(); i++) {
 			for (int j = 1; j <= s2.length(); j++) {
-				if(s1.charAt(i-1) == s2.charAt(j-1)) { // 같은 문자라면 첫번째 문자 이전 문자와 두번째 문자 이전 문자 값에 +1
+				if(s1.charAt(i-1) == s2.charAt(j-1)) { // 같은 문자라면 첫번째 문자 이전 문자열과 두번째 문자 이전 문자열의 부분 수열 길이에 +1
 					sub[i][j] = sub[i-1][j-1] + 1;
 				}
 				else {
-					sub[i][j] = Math.max(sub[i-1][j], sub[i][j-1]); // // 다른 문자라면 첫번째 문자 이전 문자와 두번째 문자 값, 첫번째 문자와 두번째 문자 이전 문자 값 중 최댓값
+					sub[i][j] = Math.max(sub[i-1][j], sub[i][j-1]); // // 다른 문자라면 첫번째 문자 이전 문자열과 두번째 문자까지의 문자열의 부분 수열 길이, 첫번째 문자까지의 문자열과 두번째 이전 문자열의 부분 수열 길이 중 최댓값
 				}
 			}
 		}
