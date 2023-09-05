1c9ac6fa98da4cac3a0586151974cb567e039aba
Keesung <person813@gmail.com>
2023-09-05 22:56:32 +0900

    프로그래머스_1843_DP(답지)

--- 20230901/python/keesung.py
+++ 20230901/python/keesung.py
@@ -1,24 +1,30 @@
 def solution(arr):
     arr = [int(arr[x]) if x % 2 == 0 else arr[x] for x in range(len(arr))]
-    max_dp = [0] * (len(arr) // 2 + 1)
-    min_dp = [0] * (len(arr) // 2 + 1)
+    INF = 10e9
+    max_dp = [[-INF] * (len(arr) // 2 + 1) for _ in range(len(arr) // 2 + 1)]
+    min_dp = [[INF] * (len(arr) // 2 + 1) for _ in range(len(arr) // 2 + 1)]
     
-    max_dp[-1] = arr[-1]
-    min_dp[-1] = arr[-1]
+    for i in range(len(arr) // 2 + 1):
+        max_dp[i][i] = arr[i * 2]
+        min_dp[i][i] = arr[i * 2]
     
-    for i in range(len(arr) // 2 - 1, -1, -1):
-        index = i * 2
-        text = index + 1
-        if text == "+":
-            max_dp[i] += max_dp[i+1]  + arr[index]
-            min_dp[i] += min_dp[i+1]  + arr[index]
-        else:
-            max_dp[i] = arr[index] - min_dp[i+1]
-            min_dp[i] = arr[index] - max_dp[i+1]
-    answer = max_dp[0]
-    print(max_dp)
-    print(min_dp)    
+    for cnt in range(1, len(arr) // 2+1):
+        for i in range(len(arr) // 2+1):
+        # for j in range(i+1, len(arr) // 2 + 1):
+            j = i + cnt
+            if j > len(arr) // 2:
+                continue
+            for k in range(i, j):
+                if arr[k*2+1] == '+':
+                    max_dp[i][j] = max(max_dp[i][j], max_dp[i][k] + max_dp[k+1][j])
+                    min_dp[i][j] = min(min_dp[i][j], min_dp[i][k] + min_dp[k+1][j])
+                else:
+                    max_dp[i][j] = max(max_dp[i][j], max_dp[i][k] - min_dp[k+1][j])
+                    min_dp[i][j] = min(min_dp[i][j], min_dp[i][k] - max_dp[k+1][j])
+    answer = max_dp[0][-1]
+    # print(max_dp)
+    # print(min_dp)
     return answer
 
 
-print(solution(["1", "-", "3", "+", "5", "-", "8"]))
+# print(solution(["1", "-", "3", "+", "5", "-", "8"]))
