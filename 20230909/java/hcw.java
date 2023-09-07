487f08d808233826ca4723a81b68f808c98a4a21
Hong Chan Woo(홍찬우) <72719325+HCW-code@users.noreply.github.com>
2023-09-07 10:51:32 +0900

    프로그래머스_92344_누적

--- 20230909/java/hcw.java
+++ 20230909/java/hcw.java
@@ -3,31 +3,37 @@ class Solution {
     public int solution(int[][] board, int[][] skill) {
         
         
-        int[][] arr = new int[board.length][board[0].length];
+        int[][] arr = new int[board.length+1][board[0].length+1];
         
-        for(int i  = 0; i< skill.length; i++){
-            
+        for(int i  = 0; i < skill.length; i++){
                 if(skill[i][0] == 1){ //공격
-                    for(int r = skill[i][1]; r<= skill[i][3]; r++){
-                        for(int c = skill[i][2]; c <= skill[i][4]; c++){
-                            arr[r][c] -= skill[i][5];
-                        }
-                    }
+                    arr[skill[i][1]][skill[i][2]] -= skill[i][5];
+                    arr[skill[i][3]+1][skill[i][4]+1] -= skill[i][5];
+                    arr[skill[i][3]+1][skill[i][2]] += skill[i][5];
+                    arr[skill[i][1]][skill[i][4]+1] += skill[i][5];
                 }else if(skill[i][0] == 2){ //회복
-                    for(int r = skill[i][1]; r<= skill[i][3]; r++){
-                        for(int c = skill[i][2]; c <= skill[i][4]; c++){
-                            arr[r][c] += skill[i][5];
-                        }
-                    }
-                } 
+                    arr[skill[i][1]][skill[i][2]] += skill[i][5];
+                    arr[skill[i][3]+1][skill[i][4]+1] += skill[i][5];
+                    arr[skill[i][3]+1][skill[i][2]] -= skill[i][5];
+                    arr[skill[i][1]][skill[i][4]+1] -= skill[i][5];
+                }
+        }
+        
+        for(int i =0 ; i < arr.length; i++){
+            for(int j =0; j< arr[0].length - 1; j++){
+                arr[i][j+1] += arr[i][j];
+            }
+        }
+        
+         for(int i =0 ; i < arr[0].length; i++){
+            for(int j =0; j< arr.length - 1; j++){
+                arr[j+1][i] += arr[j][i];
+            }
         }
         int answer = 0;
-        // for(int i =0; i< arr.length; i++){
-        //     System.out.println(Arrays.toString(arr[i]));
-        // }
         
-        for(int i =0; i< arr.length; i++){
-            for(int j =0; j< arr[i].length; j++){
+        for(int i =0; i< board.length; i++){
+            for(int j =0; j< board[i].length; j++){
                 if(board[i][j] + arr[i][j] > 0){
                     answer++;
                 }
@@ -39,26 +45,22 @@ class Solution {
     }
 }
 
-              정확성  테스트
-테스트 1 〉	통과 (0.03ms, 70.7MB)
-테스트 2 〉	통과 (0.04ms, 77.3MB)
-테스트 3 〉	통과 (0.07ms, 71.3MB)
-테스트 4 〉	통과 (0.19ms, 75.3MB)
-테스트 5 〉	통과 (0.47ms, 71MB)
-테스트 6 〉	통과 (0.57ms, 77.8MB)
-테스트 7 〉	통과 (1.43ms, 74.3MB)
-테스트 8 〉	통과 (2.27ms, 80.1MB)
-테스트 9 〉	통과 (2.59ms, 94MB)
-테스트 10 〉	통과 (4.54ms, 90MB)
+
+테스트 1 〉	통과 (0.04ms, 75.9MB)
+테스트 2 〉	통과 (0.04ms, 72MB)
+테스트 3 〉	통과 (0.09ms, 72.5MB)
+테스트 4 〉	통과 (0.11ms, 73.3MB)
+테스트 5 〉	통과 (0.26ms, 82.7MB)
+테스트 6 〉	통과 (0.29ms, 73.6MB)
+테스트 7 〉	통과 (0.58ms, 75.1MB)
+테스트 8 〉	통과 (0.45ms, 81.4MB)
+테스트 9 〉	통과 (0.93ms, 74MB)
+테스트 10 〉	통과 (1.41ms, 74.9MB)
 효율성  테스트
-테스트 1 〉	실패 (시간 초과)
-테스트 2 〉	실패 (시간 초과)
-테스트 3 〉	실패 (시간 초과)
-테스트 4 〉	실패 (시간 초과)
-테스트 5 〉	실패 (시간 초과)
-테스트 6 〉	실패 (시간 초과)
-테스트 7 〉	실패 (시간 초과)
-채점 결과
-정확성: 53.8
-효율성: 0.0
-합계: 53.8 / 100.0
+테스트 1 〉	통과 (59.27ms, 215MB)
+테스트 2 〉	통과 (67.44ms, 212MB)
+테스트 3 〉	통과 (65.20ms, 216MB)
+테스트 4 〉	통과 (59.54ms, 211MB)
+테스트 5 〉	통과 (48.54ms, 218MB)
+테스트 6 〉	통과 (78.14ms, 217MB)
+테스트 7 〉	통과 (54.75ms, 219MB)
