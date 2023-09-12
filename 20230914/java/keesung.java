10ce976984130b7401796028b27ba8f45f784e1d
keesung <person813@gmail.com>
2023-09-12 14:17:47 +0900

    백준_17825_구현

---
+++ 20230914/java/keesung.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class keesung {
+
+    public static class Node {
+        int special_check = 0;
+
+    }
+    
+	public static void main(String[] args) throws IOException {
+        
+	}
+}
+
