da12740b135f3826368892d07bc4f40a17621f52
Keesung <person813@gmail.com>
2023-10-10 01:31:58 +0900

    백준_1039_dfs(방문처리실패)

--- 20231009/java/keeesung.java
+++ 20231009/java/keeesung.java
@@ -7,6 +7,7 @@ class Solution {
     public static int[] arr;
     public static int K;
     public static int result = -1;
+    public static HashSet[] hashSets = new HashSet[2];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,7 +16,7 @@ class Solution {
         int N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         int M = 1;
-        while (Math.pow(10, M) < N) {
+        while (Math.pow(10, M) <= N) {
             M++;
         }
         arr = new int[M];
@@ -23,19 +24,32 @@ class Solution {
             arr[i] = N % 10;
             N /= 10;
         }
-        dfs(0);
+        if (M == 1 || arr[1] == 0 && arr[0] == 0) {
+            System.out.println(-1);
+            return;
+        }
 
+        hashSets[0] = new HashSet();
+        hashSets[1] = new HashSet();
+        hashSets[1].add(N);
+        dfs(0);
         System.out.println(result);
     }
 
     public static void dfs(int index) {
-        if (index == K) {
+        System.out.println((K - index) % 2);
+        if ((K - index) % 2 == 0) {
             int num = 0;
             for (int i = 0; i < arr.length; i++) {
                 num += arr[i] * Math.pow(10, arr.length - i - 1);
             }
             result = Math.max(result, num);
-            return;
+            // if (arr[0] == 4) {
+            // System.out.println("index: " + index + " num: " + num);
+            // }
+            if (index == K) {
+                return;
+            }
         }
 
         for (int i = 0; i < arr.length; i++) {
@@ -44,7 +58,19 @@ class Solution {
                     continue;
                 }
                 swap(i, j);
-                dfs(index + 1);
+                int num = 0;
+                for (int k = 0; k < arr.length; k++) {
+                    num += arr[k] * Math.pow(10, arr.length - k - 1);
+                }
+                // for (int k = 0; k < arr.length; k++) {
+                // System.out.print("index : " + index + " // " + arr[k] + " ");
+                // }
+                // System.out.println();
+                if (!hashSets[index % 2].contains(num)) {
+                    System.out.println("index : " + index + " // " + num);
+                    hashSets[index % 2].add(num);
+                    dfs(index + 1);
+                }
                 swap(i, j);
             }
         }
