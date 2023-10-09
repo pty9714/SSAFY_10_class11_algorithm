831d5741d8db547206c61734263834eaba356126
Keesung <person813@gmail.com>
2023-10-10 02:01:51 +0900

    백준_1039_bfs

--- 20231009/java/keeesung.java
+++ 20231009/java/keeesung.java
@@ -4,9 +4,10 @@ import java.util.*;
 
 class Solution {
 
-    public static int[] arr;
-    public static int K;
+    public static int M;
     public static int result = -1;
+
+    // public static HashSet<Integer> hashSet = new HashSet<>();
     public static HashSet[] hashSets = new HashSet[2];
 
     public static void main(String[] args) throws IOException {
@@ -14,72 +15,73 @@ class Solution {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        int M = 1;
+        int K = Integer.parseInt(st.nextToken());
+        M = 1;
         while (Math.pow(10, M) <= N) {
             M++;
         }
-        arr = new int[M];
-        for (int i = M - 1; i >= 0; i--) {
-            arr[i] = N % 10;
-            N /= 10;
-        }
-        if (M == 1 || arr[1] == 0 && arr[0] == 0) {
+
+        if (M == 1 || (M == 2 && N % 10 == 0)) {
             System.out.println(-1);
             return;
         }
 
         hashSets[0] = new HashSet();
         hashSets[1] = new HashSet();
-        hashSets[1].add(N);
-        dfs(0);
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] { 0, N });
+        hashSets[0].add(N);
+        // hashSet.add(N);
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            // System.out.println(cur[0] + " " + cur[1]);
+            if (((K - cur[0]) % 2 == 0)) {
+                result = Math.max(result, cur[1]);
+                if (cur[0] == K) {
+                    continue;
+                }
+            }
+            for (int i = 0; i < M; i++) {
+                for (int j = i + 1; j < M; j++) {
+                    int newNum = swap(cur[1], i, j);
+                    if (newNum == -1) {
+                        continue;
+                    }
+                    HashSet tmp = hashSets[(cur[0] + 1) % 2];
+                    if (tmp.contains(newNum)) {
+                        continue;
+                    }
+                    tmp.add(newNum);
+                    q.offer(new int[] { cur[0] + 1, newNum });
+                }
+            }
+        }
         System.out.println(result);
     }
 
-    public static void dfs(int index) {
-        System.out.println((K - index) % 2);
-        if ((K - index) % 2 == 0) {
-            int num = 0;
-            for (int i = 0; i < arr.length; i++) {
-                num += arr[i] * Math.pow(10, arr.length - i - 1);
-            }
-            result = Math.max(result, num);
-            // if (arr[0] == 4) {
-            // System.out.println("index: " + index + " num: " + num);
-            // }
-            if (index == K) {
-                return;
-            }
+    private static int swap(int N, int i, int j) {
+        int newNum = 0;
+        int[] arr = new int[M];
+        for (int k = M - 1; k >= 0; k--) {
+            arr[k] = N % 10;
+            N /= 10;
         }
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] == 0 && i == 0) {
-                    continue;
-                }
-                swap(i, j);
-                int num = 0;
-                for (int k = 0; k < arr.length; k++) {
-                    num += arr[k] * Math.pow(10, arr.length - k - 1);
-                }
-                // for (int k = 0; k < arr.length; k++) {
-                // System.out.print("index : " + index + " // " + arr[k] + " ");
-                // }
-                // System.out.println();
-                if (!hashSets[index % 2].contains(num)) {
-                    System.out.println("index : " + index + " // " + num);
-                    hashSets[index % 2].add(num);
-                    dfs(index + 1);
-                }
-                swap(i, j);
-            }
+        if (arr[j] == 0 && i == 0) {
+            return -1;
         }
-    }
-
-    private static void swap(int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+        for (int k = 0; k < arr.length; k++) {
+            newNum += arr[k] * Math.pow(10, arr.length - k - 1);
+        }
+        return newNum;
+
     }
 
 }
+
+// 메모리 13120 시간 112
+// 예외 케이스 체크 필요
+// 방문 처리 나눠서 하기, 홀수일 때 짝수일 때
+// 최대 방문 - index가 짝수일 경우 같은 숫자만 계속 바꾸면 도달할 수 있으므로 그 상황에서 바로 업데이트 해줌
