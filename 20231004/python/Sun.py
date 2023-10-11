e1136befc4281257c8969c121e92cde24b800f3f
pty9714 <pty9714@naver.com>
2023-10-11 14:21:10 +0900

    백준_1034_구현

---
+++ 20231004/python/Sun.py
@@ -0,0 +1,11 @@
+graph = []
+#그래프 만들기
+
+TC = int(input())
+
+for tc in range(1,TC+1):
+    a,b = map(int,input().split())
+    answer = 0
+    # a에서 b로 그래프 탐색
+
+    print(f'#{tc} {answer}')
--- 20231006/python/Sun.py
+++ 20231006/python/Sun.py
@@ -1,1 +1,21 @@
 n,m = map(int,input().split())
+l = []
+for i in range(n):
+    l.append(list(input()))
+
+k = int(input())
+answer = []
+for i in l:
+    cnt = i.count('0') 
+    if cnt <=k and cnt%2==k%2:
+        a = 0
+        for j in l:
+            if i==j:
+                a+=1
+        
+        answer.append(a)
+
+if answer:
+    print(max(answer))
+else:
+    print(0)
