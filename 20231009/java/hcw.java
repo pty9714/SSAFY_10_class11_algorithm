0f544552671f117c87b6e8a1eef170f41ecc6702
developer <72719325+HCW-code@users.noreply.github.com>
2023-10-11 09:20:22 +0900

    백준_1039_답지

--- 20231009/java/hcw.java
+++ 20231009/java/hcw.java
@@ -1,91 +1,84 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Trade {
+    int num;
+    int cnt;
+	
+    public Trade(int num, int cnt) {
+        this.num = num;
+        this.cnt = cnt;
+    }
+}
 
 public class Main {
 
-    public static int[] visited = new int[1000001];
-
+    static int N, K;
+    static int result = -1; // K번 연산을 할 수 없는 경우 -1을 출력하도록 -1로 초기화
+	
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N, K;
-        Queue<Integer> list = new LinkedList<>();
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        // 입력
+		
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        int length = 0;
-
-        while (N / (int) Math.pow(10, length) != 0) {
-            length++;
-        }
-
-        // queue에 처음 N 추가
-        list.add(N);
-
-        while (!list.isEmpty() && K > 0) {
-            int size = list.size();
-
-            for (int x = 0; x < size; x++) {
-                int now = list.poll();
-
-                // change
-                for (int i = 0; i < length - 1; i++) {
-                    for (int j = i + 1; j < length; j++) {
-                        int next = swap(now, i, j);
-
-                        // 앞자리가 0이거나 현재 depth에서 방문한 적이 있는 경우 queue에 add하지 않음.
-                        if (next != 0 && visited[next]!=K) {
-                            list.add(next);
-                            visited[next] = K;
-                        }
+        bfs();
+        System.out.println(result);
+		
+    }
+	
+    public static void bfs() {
+        Queue<Trade> q = new LinkedList<>();
+        boolean[][] visited = new boolean[1000001][K + 1];
+		
+        q.add(new Trade(N, 0));
+        visited[N][0] = true;
+		
+        while (!q.isEmpty()) {
+            Trade t = q.poll();
+			
+            // 교환 횟수가 K일 경우 최댓값 갱신 후 다음 숫자로 넘어감
+            if (t.cnt == K) {
+                result = Math.max(result, t.num);
+                continue;
+            }
+			
+            int len = String.valueOf(t.num).length();
+			
+            for (int i = 0; i < len - 1; i++) {
+                for (int j = i + 1; j < len; j++) {
+                    int next = swap(t.num, i , j);
+					
+                    if (next != -1 && !visited[next][t.cnt + 1]) {
+                        q.add(new Trade(next, t.cnt + 1));
+                        visited[next][t.cnt + 1] = true;
                     }
                 }
             }
-
-            K--;
         }
-
-        if (list.isEmpty()) {
-            System.out.println("-1");
-        } else {
-
-            int ans = 0;
-
-            for (int x : list) {
-                ans = Math.max(x, ans);
-            }
-
-            System.out.println(ans);
+		
+    }
+	
+    // i번 위치의 숫자와 j번 위치의 숫자를 바꿈
+    public static int swap(int n, int i, int j) {
+        char[] numArr = String.valueOf(n).toCharArray();
+	
+        // i가 0이고 j번 위치의 숫자가 0이라면 숫자를 바꿨을 때 0으로 시작하게 되기 때문에 -1을 return
+        if (i == 0 && numArr[j] == '0') {
+            return -1;
         }
-
-
+		
+        char tmp;
+        tmp = numArr[i];
+        numArr[i] = numArr[j];
+        numArr[j] = tmp;
+		
+        return Integer.parseInt(new String(numArr));
     }
-
-    // 교환
-    public static int swap(int now, int i, int j) {
-
-
-        char[] list = String.valueOf(now).toCharArray();
-
-        char temp = list[i];
-        list[i] = list[j];
-        list[j] = temp;
-
-
-        // 앞자리가 0이면 error
-
-        if(list[0]=='0')
-            return 0;
-
-
-        return Integer.parseInt(new String(list));
-    }
-
 
 }
