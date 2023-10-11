399e46ebd5111a9997c67db67bd8d43407d0dae4
Park KIRyun <62732145+parkgr95@users.noreply.github.com>
2023-10-11 10:36:26 +0900

    프로그래머스_양궁대회_중복조합 or dfs

--- 20231007/python/giryun.py
+++ 20231007/python/giryun.py
@@ -1,8 +1,6 @@
 from copy import deepcopy
 
-
 max_diff, max_board = 0, []
-
 
 def solution(n, info):
     def dfs(ascore, lscore, cnt, idx, board):
@@ -20,7 +18,6 @@ def solution(n, info):
             board2 = deepcopy(board)
             board2[10 - idx] = info[10 - idx] + 1
             dfs(ascore, lscore + idx, cnt + info[10 - idx] + 1, idx + 1, board2)
-            
         board1 = deepcopy(board)
         if info[10 - idx] > 0:
             dfs(ascore + idx, lscore, cnt, idx + 1, board1)            
@@ -57,3 +54,55 @@ def solution(n, info):
 테스트 24 〉	통과 (20.28ms, 10.4MB)
 테스트 25 〉	통과 (13.70ms, 10.1MB)
 """
+
+# 이전 코드
+from itertools import combinations_with_replacement
+from collections import Counter
+
+def solution(n, info):
+    answer = []
+    maxDiff, maxComb = 0, {}
+    for cb in combinations_with_replacement(range(11), n):
+        cnt = Counter(cb)
+        lscore, ascore = 0, 0
+        for i in range(11):
+            if info[10-i] < cnt[i]: lscore += i
+            elif info[10-i] > 0: ascore += i
+        diff = lscore - ascore
+        if diff > maxDiff:
+            maxDiff = diff
+            maxComb = cnt
+    if maxDiff > 0:
+        answer = [0] * 11
+        for n in maxComb:
+            answer[10-n] = maxComb[n]
+        return answer
+    else: return [-1]
+        
+"""
+테스트 1 〉	통과 (0.44ms, 10.3MB)
+테스트 2 〉	통과 (349.91ms, 10.2MB)
+테스트 3 〉	통과 (349.36ms, 10.2MB)
+테스트 4 〉	통과 (11.21ms, 10.1MB)
+테스트 5 〉	통과 (776.39ms, 10.2MB)
+테스트 6 〉	통과 (755.51ms, 9.95MB)
+테스트 7 〉	통과 (11.15ms, 10.4MB)
+테스트 8 〉	통과 (1.10ms, 10.2MB)
+테스트 9 〉	통과 (11.94ms, 10.2MB)
+테스트 10 〉	통과 (1.12ms, 10.3MB)
+테스트 11 〉	통과 (5.42ms, 10.2MB)
+테스트 12 〉	통과 (4.40ms, 10.2MB)
+테스트 13 〉	통과 (104.02ms, 10.2MB)
+테스트 14 〉	통과 (381.17ms, 10.2MB)
+테스트 15 〉	통과 (420.39ms, 10.1MB)
+테스트 16 〉	통과 (51.35ms, 10.2MB)
+테스트 17 〉	통과 (16.45ms, 10.2MB)
+테스트 18 〉	통과 (0.28ms, 10.1MB)
+테스트 19 〉	통과 (0.07ms, 10.2MB)
+테스트 20 〉	통과 (464.62ms, 10.2MB)
+테스트 21 〉	통과 (536.96ms, 10.2MB)
+테스트 22 〉	통과 (887.47ms, 10.4MB)
+테스트 23 〉	통과 (1.95ms, 10.1MB)
+테스트 24 〉	통과 (781.94ms, 10.1MB)
+"""
+테스트 25 〉	통과 (1076.14ms, 10.3MB)
