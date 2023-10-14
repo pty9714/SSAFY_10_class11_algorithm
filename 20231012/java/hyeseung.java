166caa586e14b2dc4cff34c217c65f0cbe466567
신혜승 <hyeseung2000@gmail.com>
2023-10-14 23:38:22 +0900

    백준_20444_이진탐색

--- 20231012/java/hyeseung.java
+++ 20231012/java/hyeseung.java
@@ -1,33 +1,46 @@
-import java.util.*;
- 
-public class Main {
- 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
- 
-        long n = scan.nextInt();
-        long k = scan.nextLong();
- 
-        long left = 0;
-        long right = n / 2;
-        while(left <= right) {
-            long row = (left + right) / 2;
-            long col = n - row;
- 
-            long total = cut_paper(row, col);
-            if(total == k) {
-                System.out.println("YES");
-                return;
-            } else if(total > k) { //row col의 차이가 더 커야한다.
-                right = row - 1;
-            } else if (total < k){
-                left = row + 1;
-            }  
-        }
-        System.out.println("NO");
-    }
- 
-    public static long cut_paper(long row, long col) {
-        return (row + 1) * (col + 1);
-    }
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 	11600KB, 80ms
+public class B20444 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String answer = "NO";
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long N = Long.parseLong(st.nextToken());
+		long K = Long.parseLong(st.nextToken());
+		
+		long left = 0;
+		long right = N / 2;
+		while(left <= right) {
+			long row = (left + right) / 2;
+			long col = N - row;
+			long temp = (row + 1) * (col + 1);
+			if(temp == K) {
+				answer = "YES";
+				break;
+			}
+			else if(temp > K) {
+				right = row - 1;
+			}
+			else if(temp < K) {
+				left = row + 1;
+			}
+		}
+		
+		bw.write(answer + "");
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
 }
