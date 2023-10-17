664031249df804ebc0e535bb0b409e0085692eb8
Keesung <person813@gmail.com>
2023-10-18 00:52:56 +0900

    백준_1005_메모이제이션(dp)

--- 20231011/java/keesung.java
+++ 20231011/java/keesung.java
@@ -7,6 +7,7 @@ import java.util.StringTokenizer;
 class Solution {
 
     public static int[] memorize;
+    public static boolean[] calc;
     public static int[] costs;
     public static ArrayList<ArrayList<Integer>> buildingArray;
 
@@ -14,11 +15,13 @@ class Solution {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
         for (int test_case = 0; test_case < T; test_case++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
             memorize = new int[N + 1];
+            calc = new boolean[N + 1];
             buildingArray = new ArrayList<>(N + 1);
 
             costs = new int[N + 1];
@@ -37,13 +40,14 @@ class Solution {
             int W = Integer.parseInt(br.readLine());
 
             int result = dfs(W);
-
-            System.out.println(result);
+            sb.append(result);
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 
     public static int dfs(int index) {
-        if (memorize[index] != 0) {
+        if (calc[index]) {
             return memorize[index];
         }
         for (int parent : buildingArray.get(index)) {
@@ -53,7 +57,11 @@ class Solution {
             }
         }
         memorize[index] += costs[index];
+        calc[index] = true;
         return memorize[index];
     }
 
 }
+
+// 메모리 242996kb 시간 684ms
+// 메모이제이션, dp, 계산값 0이 될 경우 주의
