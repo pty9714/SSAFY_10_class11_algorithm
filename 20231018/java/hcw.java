aa6df525c98f3b26eee0d7afea7a94adefb2ed62
developer <72719325+HCW-code@users.noreply.github.com>
2023-10-19 00:08:00 +0900

    백준_9466_dfs

--- 20231018/java/hcw.java
+++ 20231018/java/hcw.java
@@ -7,25 +7,21 @@ public class Main {
 	static int[] cycle;
 	static int count;
 
-	private static void dfs(int start, int current) {
-		if (visited[arr[current]] == 1 && cycle[arr[current]] == 0) { //방문되어있고 다음 번호의 사이클이 0이라면 
-			int s = current;
-			int c = 0;
-			while(cycle[s] == 0) {
-				cycle[s] = 1;
-				c++;
-				s = arr[s];
-			}
-
-			count += c;
-			return;
-		} else {
-			if(visited[arr[current]] == 0) { //다음 곳에 방문하지 않았다면 
-				visited[arr[current]] = 1;
-				dfs(start, arr[current]);
+	private static void dfs(int current) {
+		visited[current] = 1;
+		int next = arr[current];
+		if(visited[next] == 0) {
+			dfs(next);
+		}else {
+			if(cycle[next] == 0) {
+				count++;
+				while(next != current) {
+					count++;
+					next = arr[next];
+				}
 			}
 		}
-
+		cycle[current] = 1;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -45,18 +41,13 @@ public class Main {
 			visited = new int[num + 1];
 			count = 0;
 			for (int i = 1; i < num + 1; i++) {
-				
-				if (cycle[i] == 0 && cycle[arr[i]] == 0) { //사이클이 비어있다면
-					visited[arr[i]] = 1;
-					dfs(i, arr[i]);
-
-				}
+				dfs(i);
 			}
-			sb.append(num - count + "\n");
+
+			sb.append((num - count) + "\n");
 		}
 		System.out.println(sb);
 
 	}
 
 }
-
