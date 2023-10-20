2f20299202bdcac9172eecb23ebe675d09761478
Keesung <person813@gmail.com>
2023-10-20 09:37:46 +0900

    프로그래머스_138475_구현(답지)

---
+++ 20231020/java/keesung.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] solution(int e, int[] starts) {
+        int[] answer = new int[starts.length];
+        int[] dic = new int[e+1];
+        int[] cnt = new int[e+1];
+        dic[1] = 1;
+        cnt[1] = 1;
+        for(int i=2;i<=e;i++){
+            int n = e/i;
+            int j = i;
+            for(int k=1;k<=n;k++){
+                dic[j] += 1;
+                j += i;
+            }
+        }
+
+        // e부터 1까지 각각 최대 출연 번호 저장
+        int v = 0;
+        for(int i=e;i>=1;i--){
+            if(dic[i] >= v){
+                v = dic[i];
+                cnt[i] = i;
+            } else{
+                cnt[i] = cnt[i+1];
+            }
+        }
+
+        for(int i=0;i<starts.length;i++){
+            answer[i] = cnt[starts[i]];
+        }
+        return answer;
+    }
+}
+// 답지
