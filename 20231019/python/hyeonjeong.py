775627913a3023a805a1164c796de51025d59270
noname <69911502+smarth2o@users.noreply.github.com>
2023-10-20 15:49:55 +0900

    백준_10776_dfs실패

--- 20231019/python/hyeonjeong.py
+++ 20231019/python/hyeonjeong.py
@@ -1,31 +1,33 @@
 # 실패
-
-def dfs(s, e, t, h):
+def dfs(i, t, h):
     global result
-    if h >= K:
-        return
-    if e == end and (result < 0 or result > t):
+    # 도착점에 도착
+    if i == end:
         result = t
         return
-
-    for nxt in conn[e]:
-        dfs(e, nxt, t + roads[e][nxt][0], h + roads[e][nxt][1])
+    # 연결되어 있는 섬 중에 방문하지 않았고, 최단 시간을 넘지 않으며, 뗏목이 사라지지 않는 섬만 방문
+    for nxt in roads[i]:
+        if visited[nxt[0]] or t + nxt[1] >= result > -1 or h + nxt[2] >= K:
+            continue
+        visited[nxt[0]] = True
+        dfs(nxt[0], t + nxt[1], h + nxt[2])
+        visited[nxt[0]] = False
 
 
 K, N, M = map(int, input().split())
-roads = [[() for _ in range(N + 1)] for _ in range(N + 1)]
-conn = [[] for _ in range(N + 1)]
+roads = [[] for _ in range(N + 1)]
+visited = [False for _ in range(N+1)]
 
 for _ in range(M):
     a, b, time, height = map(int, input().split())
-    roads[a][b] = (time, height)
-    conn[a].append(b)
+    roads[a].append([b, time, height])
+    roads[b].append([a, time, height])
 
 start, end = map(int, input().split())
 result = -1
 
-
-for i in conn[start]:
-    dfs(start, i, roads[start][i][0], roads[start][i][1])
+dfs(start, 0, 0)
 
 print(result)
+
+# 52% pypy3 63%
