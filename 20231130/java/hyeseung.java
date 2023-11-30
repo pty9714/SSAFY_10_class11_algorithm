c5be5f4a1ad8341b8eaf4e25d64186846e01fe50
신혜승 <hyeseung2000@gmail.com>
2023-11-30 17:57:36 +0900

    백준_2470_투포인터

---
+++ 20231130/java/hyeseung.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 31684KB, 304ms
+public class B2470 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		long sum = Long.MAX_VALUE;
+		long[] ans = new long[2];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long[] solution = new long[N];
+		
+		for (int i = 0; i < N; i++) {
+			solution[i] = Long.parseLong(st.nextToken());
+		}
+		
+		Arrays.sort(solution);
+		
+		int alkali = 0;
+		int acid = N - 1;
+		
+		while(alkali < N && acid >= 0 && alkali != acid) {
+			if(Math.abs(solution[acid] + solution[alkali]) < Math.abs(sum)) {
+				sum = solution[acid] + solution[alkali];
+				ans[0] = solution[alkali];
+				ans[1] = solution[acid];
+			}
+			if(solution[acid] + solution[alkali] < 0) alkali++;
+			else acid--;
+		}
+		
+		Arrays.sort(ans);
+		bw.write(ans[0] + " " + ans[1]);
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
