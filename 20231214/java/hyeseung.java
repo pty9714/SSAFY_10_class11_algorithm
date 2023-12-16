7a932d8e7de7a34f9660f03fc513a95739ad2d45
신혜승 <hyeseung2000@gmail.com>
2023-12-16 20:15:34 +0900

    백준_1644_투포인터

--- 20231214/java/hyeseung.java
+++ 20231214/java/hyeseung.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
+// 25488KB, 240ms
 public class B1644 {
 
 	public static void main(String[] args) throws IOException {
@@ -14,35 +15,32 @@ public class B1644 {
 		int N = Integer.parseInt(br.readLine());
 		int ans = 0;
 		
-		boolean check[] = new boolean[N+1];
-        ArrayList<Integer> primes = new ArrayList<>();
+		boolean primeCheck[] = new boolean[N + 1];
+        ArrayList<Integer> prime = new ArrayList<>();
 
         // 소수가 아니면 true로
-        check[0] = check[1] = true;
+        primeCheck[0] = primeCheck[1] = true;
 
-        for (int i = 2; i <= Math.sqrt(check.length); i++) {
-            for (int j = i*i; j <= N; j += i){
-                if(!check[j]) check[j] = true;
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            for (int j = i + i; j <= N; j += i){
+                primeCheck[j] = true;
             }
         }
         for (int i = 0; i <= N; i++){
-            if(!check[i]) primes.add(i); // 소수 넣기
+            if(!primeCheck[i]) prime.add(i); // 소수 배열 만들기
         }
 
         int sum = 0, left = 0;
-
-        for (int right = 0; right < N; right++){
-            if(right >= primes.size()) continue;
-
-            sum += primes.get(right);
-
-            if(sum == N) ans++;
-
-            while(sum >= N && left >= 0){ // sum 보다 같거나 작으면 left 증가해 해당 값 빼줌
-                sum -= primes.get(left++);
-                if(sum == N) ans++;
-            }
-        }		
+        
+        for (int right = 0; right < prime.size(); right++) {
+			sum += prime.get(right);
+			if(sum == N) ans++;
+			while(sum >= N) {
+				sum -= prime.get(left++);
+				if(sum == N) ans++;
+			}
+		}
+	
 		bw.write(ans + "");
 		bw.flush();
 		bw.close();
@@ -50,3 +48,8 @@ public class B1644 {
 	}
 
 }
+/*
+ 에라토스테네스의 체 O(N^1/2)
+ 임의의 수 n까지의 소수를 구하고자 할 때 2부터 제곱근까지 돌며 모든 배수들을 소수에서 제외시키는 방식
+ -> 미리 소수 구해서 투포인터로 해결함
+ */
