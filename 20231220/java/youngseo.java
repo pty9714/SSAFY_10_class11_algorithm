6243db60e4d1ec41fdf7dd334254bb8d5899d354
youngseo9603 <youngseo9603@gmail.com>
2023-12-27 15:33:25 +0900

    백준_1967_dfs

---
+++ 20231220/java/youngseo.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BaekJoon1967 {
+    static List<Node>[] list;
+    static boolean[] visited;
+    static int n;
+    static int answer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        list = new List[n+1];
+        for(int i=0; i<n+1; i++)
+            list[i] = new ArrayList<>();
+        visited = new boolean[n+1];
+        StringTokenizer st;
+        for(int i=0; i<n-1; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            list[x].add(new Node(y,k));
+            list[y].add(new Node(x,k));
+        }
+        answer = 0;
+        for(int i=1; i<n+1; i++){
+            visited[i] = true;
+            dfs(i, 0);
+            visited[i] = false;
+        }
+
+        System.out.println(answer);
+
+    }
+
+    public static void dfs(int cur, int sum){
+        answer = Math.max(answer, sum);
+
+        for(Node node : list[cur]){
+            if(!visited[node.num]){
+                visited[node.num] = true;
+                dfs(node.num, sum+node.len);
+                visited[node.num] = false;
+            }
+        }
+    }
+
+    static class Node{
+        int num;
+        int len;
+
+        Node(int num, int len){
+            this.len = len;
+            this.num = num;
+        }
+    }
+}
+
+/*
+    map을 int형 2차원 배열로 선언했더니 메모리 초과
+    list로 선언해서 성공
+    32772KB 	2616ms
+ */
