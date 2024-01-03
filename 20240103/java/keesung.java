dc16b9a5fec670277b9f95b3c5a8d7675670033a
Keesung <person813@gmail.com>
2024-01-04 08:20:34 +0900

    프로그래머스_13.29ms_누적합

--- 20240103/java/keesung.java
+++ 20240103/java/keesung.java
@@ -1,4 +1,6 @@
-class Solution {
+
+public class Solution {
+
     public long solution(int[] sequence) {
         long answer = 0;
 
@@ -12,15 +14,21 @@ class Solution {
                 dp[i] = dp[i - 1] + sequence[i - 1];
             }
         }
-        for (int i = 1; i < dp.length; i++) {
-            for (int j = i; j < dp.length; j++) {
-                long tmp = Math.abs(dp[j] - dp[i - 1]);
-                if (tmp > answer) {
-                    answer = tmp;
-                }
 
-            }
+        long max = -1000000;
+        long min = 1000000;
+        for (int i = 0; i <= sequence.length; i++) {
+            max = Math.max(max, dp[i]);
+            min = Math.min(min, dp[i]);
         }
+
+        answer = Math.abs(max - min);
+
         return answer;
     }
+
 }
+
+// 누적합
+// 처음에 이중포문으로 돌렸는데, 50만 * 50만이라서 시간초과남
+// 이중포문 없이 그냥 max - min 하면 끝나는거라 포문 없애고 O(N)으로 동작하도록 변경
