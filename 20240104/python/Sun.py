adf5e6046afe0c8823f008ed7769a6854c9d7939
pty9714 <pty9714@naver.com>
2024-01-05 07:51:36 +0900

    백준 192ms dp

---
+++ 20240104/python/Sun.py
@@ -0,0 +1,13 @@
+n,k = map(int,input().split())
+coin = []
+for _ in range(n):
+    coin.append(int(input()))
+
+dp = [0] * (k+1)
+dp[0] = 1
+
+for c in coin:
+    for i in range(c,k+1):
+        dp[i] +=dp[i - coin]
+
+print(dp[k])
