ad6766f42fb62fb78ae1855cac347ac3bc1dafaa
신혜승 <hyeseung2000@gmail.com>
2024-01-21 23:03:36 +0900

    백준_146.03ms_그래프

--- 20240116/java/hyeseung.java
+++ 20240116/java/hyeseung.java
@@ -1,37 +1,36 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-// 12148KB, 112ms
-public class B2294 {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		int[] coins = new int[n];
-		for (int i = 0; i < n; i++) {
-			coins[i] = Integer.parseInt(br.readLine());
-		}		
-		int[] dp = new int[k+1];
-		Arrays.fill(dp, 10001);
-		dp[0] = 0;
-		for (int i = 1; i <= k; i++) {
-			for (int coin : coins) {
-				if(i >= coin)
-					dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-			}
-		}
-		bw.write((dp[k] == 10001 ? -1 : dp[k]) + "");
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-
+import java.util.ArrayList;
+class Solution {
+    static class Node {
+        int in;
+        int out;
+        Node(int in, int out) {
+            this.in = in;
+            this.out = out;
+        }
+    }
+    public int[] solution(int[][] edges) {
+        int[] answer = new int[4];
+        ArrayList<Node> graph = new ArrayList<Node>();
+        for(int i = 0; i <= 1000000; i++) {
+            graph.add(new Node(0, 0));
+        }
+        for(int[] edge : edges) {
+            graph.get(edge[0]).out++;
+            graph.get(edge[1]).in++;
+        }
+        for(int i = 1; i <= 1000000; i++) {
+            if(graph.get(i).in == 0 && graph.get(i).out >= 2) {
+                answer[0] = i;
+            }
+            else if(graph.get(i).out == 0 && graph.get(i).in >= 1) {
+                answer[2]++;
+            }
+            else if(graph.get(i).in >= 2 && graph.get(i).out >= 2) {
+                answer[3]++;
+            }
+        }
+        answer[1] = graph.get(answer[0]).out - answer[2] - answer[3];
+        return answer;
+    }
 }
+// 146.03ms
