639edcd18d7e156de3faf6375bb1e8c1b6a80877
keesung <person813@gmail.com>
2024-03-13 10:56:52 +0900

    백준_메모리초과_bfs

--- 20240314/java/keesung.java
+++ 20240314/java/keesung.java
@@ -1,5 +1,5 @@
+package src;
 
-package 20240314.java;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,7 +10,7 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-public class keesung {
+public class Solution {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,11 +18,14 @@ public class keesung {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
+        HashMap<Integer, Integer> visited = new HashMap<>();
+
         Queue<int[]> queue = new LinkedList<>();
 
-        queue.add(new int[] {0, N});
+        queue.add(new int[] { 0, N });
         int time = Integer.MAX_VALUE;
         int way = 0;
+        visited.put(N, 0);
         while (!queue.isEmpty()) {
             int[] data = queue.poll();
             int repeat = data[0];
@@ -38,14 +41,30 @@ public class keesung {
                 way++;
                 continue;
             }
-            queue.add(new int[] {repeat+1, point+1});
-            queue.add(new int[] {repeat+1, point-1});
-            queue.add(new int[] {repeat+1, point*2});
+            int next = repeat + 1;
+            addToQueue(queue, visited, point + 1, next);
+            addToQueue(queue, visited, point - 1, next);
+            addToQueue(queue, visited, point * 2, next);
         }
 
         System.out.println(time);
         System.out.println(way);
 
     }
-    
+
+    public static void addToQueue(Queue<int[]> queue, HashMap<Integer, Integer> visited, int point, int next) {
+        if (point < 0) {
+            return;
+        }
+        if (visited.containsKey(point) && visited.get(point) < next) {
+            return;
+        }
+        queue.add(new int[] { next, point });
+        visited.put(point, next);
+    }
+
 }
+
+// bfs
+// 각 경우의 수에 따라서 queue 활용하기
+// 방문처리 했는데도 안됨
