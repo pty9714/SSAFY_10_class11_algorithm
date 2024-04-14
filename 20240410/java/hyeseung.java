03f09538ecf153bdd156b8016f73deb7f690821d
hyeseungS <hyeseung2000@gmail.com>
2024-04-14 17:19:22 +0900

    백준_156ms_플로이도워샬

--- 20240410/java/hyeseung.java
+++ 20240410/java/hyeseung.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-
+// 17104KB, 156ms
 public class B2224 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
