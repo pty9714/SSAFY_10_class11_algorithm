520624c992cd71ee4978bb7c60705e4dbdf318c0
hyeseungS <hyeseung2000@gmail.com>
2024-04-29 15:07:59 +0900

    백준_240ms_dp

---
+++ 20240426/java/hyeseung.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+// 15104KB, 240ms
+public class B11049 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[][] matrix = new int[N + 1][2];
+        int[][] dp = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            matrix[i][0] = Integer.parseInt(st.nextToken());
+            matrix[i][1] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 1; i < N; i++) {
+            dp[i][i + 1] = matrix[i][0] * matrix[i][1] * matrix[i + 1][1];
+        }
+        for (int i = 2; i < N; i++) {
+            for (int first = 1; first + i <= N; first++) {
+                int last = i + first;
+                dp[first][last] = Integer.MAX_VALUE;
+                for (int middle = first; middle < last; middle++) {
+                    dp[first][last] = Math.min(dp[first][last], dp[first][middle] + dp[middle + 1][last] + matrix[first][0] * matrix[middle][1] * matrix[last][1]);
+                }
+            }
+        }
+        bw.write(dp[1][N] + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
