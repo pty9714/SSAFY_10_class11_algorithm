5c990740be8f6fdf4d3490015822b5a875f751fb
이가영 <tp06100@naver.com>
2024-05-06 18:50:05 +0900

    백준_답지_dfs

---
+++ 20240503/python/gayoung.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+tree = [[] for _ in range(N + 1)]
+dp = [[0, 0] for _ in range(N + 1)]
+visited = [0 for _ in range(N + 1)]
+
+def dfs(node):
+    visited[node] = 1
+    dp[node][0] = 1
+    for child_node in tree[node]:
+        if not visited[child_node]:
+            dfs(child_node)
+            dp[node][0] += min(dp[child_node][0], dp[child_node][1])
+            dp[node][1] += dp[child_node][0]
+            
+            
+for _ in range(N-1):
+    a, b = map(int, input().split())
+    tree[a].append(b)
+    tree[b].append(a)
+    
+dfs(1)
+answer = min(dp[1][0], dp[1][1])
+print(answer)
