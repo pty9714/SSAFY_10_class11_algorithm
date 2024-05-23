fcdcdce62aff0c9e0bc5d4c88f3eba9130c5970e
sycha11 <76770475+sycha11@users.noreply.github.com>
2024-05-23 14:25:20 +0900

    Create seungyun.java
    백준_124ms_유클리드호제법

---
+++ 20240523/java/seungyun.java
@@ -0,0 +1,22 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        System.out.println(m - gcd(n,m));
+    }
+
+    static int gcd(int a, int b){
+        if(b == 0) return a;
+        return gcd(b, a%b);
+    }
+
+}
