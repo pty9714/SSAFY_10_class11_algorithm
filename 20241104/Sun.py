3631414e68c52c58999c535829a305644668600d
pty9714 <pty9714@naver.com>
2024-11-11 06:39:32 +0900

    백준_40ms_구현

---
+++ 20241104/Sun.py
@@ -0,0 +1,39 @@
+n, l = map(int, input().split())
+l = [list(map(int, input().split())) for _ in range(n)]
+
+def cr(road):
+    global n, l
+    visited = [0] * n
+    for i in range(n - 1):
+        if road[i] != road[i + 1]:
+            if abs(road[i] - road[i+1]) > 1 :
+                return False
+            else:
+                if road[i] - road[i + 1] == 1:
+                    if i+1+l > n:
+                        return False
+                    check = road[i + 1]
+                    for j in range(i+1, i+1+l):
+                        if visited[j] or road[j] != check:
+                            return False
+                        visited[j] = 1
+                elif road[i] - road[i + 1] == -1:
+                    if i - l < - 1:
+                        return False
+                    check = road[i]
+                    for j in range(i, i-l, -1):
+                        if visited[j] or road[j] != check:
+                            return False
+                        visited[j] = 1
+    return True
+
+result = 0
+for row in l:
+    if cr(row):
+        result += 1
+
+for columns in range(n):
+    if cr( [l[row][columns] for row in range(n)]):
+        result += 1
+
+print(result)
